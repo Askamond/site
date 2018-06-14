0439be6cfea789c4036f78dc739c82c170a997bc
Igor M <asdfgh21@mail.ru>
2018-06-14 13:18:40 +0300

    Start carnival

---
+++ CreateNewRecord/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+class Record(models.Model):
+	sex = models.BooleanField()
+	age = models.PositiveSmallIntegerField()
+	education = models.PositiveSmallIntegerField()
+	work_field = models.PositiveSmallIntegerField()
+	work_group = models.PositiveSmallIntegerField()
+	otnoshenie_k_ekonomike = models.PositiveSmallIntegerField()
+	start_date = models.DateTimeField(
+		default = timezone.now)
+
+	def GetInfoFromDB(self):
+		self.save()
+
+	def SendInfoToDB(self):
+		self.save()
