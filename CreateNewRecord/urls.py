3ee06cde493a825b55dfb8a1d44a6033ba2020ec
Igor M <asdfgh21@mail.ru>
2018-06-18 12:35:30 +0300

    Yeeee

--- CreateNewRecord/urls.py
+++ CreateNewRecord/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.record_list, name='record_list'),
+    url(r'^\d+$', views.check_this, name = 'check_this'),
 ]
--- CreateNewRecord/views.py
+++ CreateNewRecord/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 def record_list(request):
     return render(request, 'CreateNewRecord/record_list.html', {})
 
+def check_this(request):
+    return render(request, 'CreateNewRecord/check_this.html')
 
 def post(self, request):
     return request.post.__str__
