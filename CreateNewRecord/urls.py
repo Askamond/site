10cc233df92e3dc45e1d27872b70da80b8cf7a26
Kirill Zh <kej1@mail.ru>
2018-06-23 15:13:47 +0300

    Creating separate branch

--- CreateNewRecord/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.record_list, name='record_list'),
-    url(r'^check_this$', views.post, name = 'check_this'),
-]
