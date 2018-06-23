4f5ee654fc593ee84bce9c9c4c65515e9d21c804
Kirill Zh <kej1@mail.ru>
2018-06-24 01:07:41 +0300

    Bring live in testing page, still need some polishing. Added source files

--- scripts/testing.js
+++ scripts/testing.js
@@ -1,19 +1,99 @@
+function getCurrentQuestionNumber() {
+    var currentQuestion = document.querySelector(".right-column .question.selected");
+    var currentQuestionButton = document.querySelector(".left-column .select-button.selected");
+    if (currentQuestion === null || currentQuestionButton === null)
+        return undefined;
+    return +currentQuestion.getAttribute("question");
+}
+function getCurrentQuestionButton() {
+    var currentQuestionButton = document.querySelector(".left-column .select-button.selected");
+    return currentQuestionButton;
+}
+function getCurrentQuestionFrame() {
+    var currentQuestion = document.querySelector(".right-column .question.selected");
+    return currentQuestion;
+}
+function isNull(variable) {
+    if (variable === null || variable === NaN || variable === undefined) {
+        return true;
+    }
+    return false;
+}
+function checkAnswered() {
+    var currentQuestion = getCurrentQuestionFrame();
+    var inputs = currentQuestion.querySelectorAll("input");
+    var answered = false;
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].checked) {
+            answered = true;
+            break;
+        }
+    }
+    if (!answered) {
+        return;
+    }
+    var currentQuestionButton = getCurrentQuestionButton();
+    currentQuestionButton.classList.add("answered");
+}
 function markQuestion() {
+    var currentQuestionButton = getCurrentQuestionButton();
+    if (currentQuestionButton.classList.contains("marked")) {
+        currentQuestionButton.classList.remove("marked");
+    }
+    else {
+        currentQuestionButton.classList.add("marked");
+    }
+    checkMark();
+}
+function checkMark() {
+    var currentQuestionButton = getCurrentQuestionButton();
+    if (currentQuestionButton.classList.contains("marked")) {
+        markButton.innerText = "Снять флажок";
+    }
+    else {
+        markButton.innerText = "Отметить флажком";
+    }
 }
 function nextQuestion() {
+    var currentQuestion = getCurrentQuestionNumber();
+    goToQuestion(currentQuestion + 1);
 }
 function previousQuestion() {
+    var currentQuestion = getCurrentQuestionNumber();
+    goToQuestion(currentQuestion - 1);
 }
 function goToQuestion(number) {
+    var currentQuestion = document.querySelector(".right-column .question.selected");
+    var currentQuestionButton = document.querySelector(".left-column .select-button.selected");
+    var newQuestion = document.querySelector(".right-column .question[question='" + number + "']");
+    var newQuestionButton = document.querySelector(".left-column .select-button[question='" + number + "']");
+    if (newQuestion === null || currentQuestion === null
+        || currentQuestionButton === null || newQuestionButton === null)
+        return;
+    checkAnswered();
+    currentQuestion.classList.remove("selected");
+    currentQuestionButton.classList.remove("selected");
+    newQuestion.classList.add("selected");
+    newQuestionButton.classList.add("selected");
+    checkMark();
 }
 function endTest() {
+    var form = document.getElementsByTagName("form")[0];
+    var confirmation = confirm("Отправить результаты на проверку?");
+    if (confirmation) {
+        form.submit();
+    }
 }
-var form = document.getElementsByTagName("form")[0];
-var navigation = document.getElementById("nav-buttons");
-var buttons = navigation.getElementsByTagName("button");
+var markButton = document.getElementById("mark-question");
+markButton.onclick = markQuestion;
+var prevButton = document.getElementById("prev-question");
+var nextButton = document.getElementById("next-question");
+prevButton.onclick = previousQuestion;
+nextButton.onclick = nextQuestion;
+var buttons = document.querySelectorAll(".left-column nav .select-button");
 var _loop_1 = function (i) {
     var questionNumber = +buttons[i].getAttribute("question");
-    if (questionNumber === NaN)
+    if (isNull(questionNumber))
         return "continue";
     buttons[i].onclick = function () {
         goToQuestion(questionNumber);
@@ -22,3 +102,24 @@ var _loop_1 = function (i) {
 for (var i = 0; i < buttons.length; i++) {
     _loop_1(i);
 }
+var endTestButton = document.querySelector(".left-column .submit-button");
+endTestButton.onclick = endTest;
+var timer = document.querySelector(".left-column .timer .time-left");
+var time = 60 * 60;
+setTimeout(function timerTick() {
+    time--;
+    var minutes = Math.floor(time / 60);
+    var seconds = (time - +minutes * 60) % 60;
+    var formatter = new Intl.NumberFormat("ru", {
+        minimumIntegerDigits: 2
+    });
+    timer.innerText = "" + minutes + ":" + formatter.format(seconds);
+    if (time > 0) {
+        setTimeout(timerTick, 1000);
+    }
+    else {
+        alert("Время вышло!");
+        var form = document.getElementsByTagName("form")[0];
+        form.submit();
+    }
+}, 1000);
---
+++ scripts/testing.ts
@@ -0,0 +1,145 @@
+function getCurrentQuestionNumber() : number {
+    let currentQuestion = document.querySelector(".right-column .question.selected");
+    let currentQuestionButton = document.querySelector(".left-column .select-button.selected");
+    if (currentQuestion === null || currentQuestionButton === null)
+        return undefined;
+
+    return +currentQuestion.getAttribute("question");
+}
+
+function getCurrentQuestionButton() : Element {
+    let currentQuestionButton = document.querySelector(".left-column .select-button.selected");
+    return currentQuestionButton;
+}
+
+function getCurrentQuestionFrame() : Element {
+    let currentQuestion = document.querySelector(".right-column .question.selected");
+    return currentQuestion;
+}
+
+function isNull(variable : any) : Boolean {
+    if (variable === null || variable === NaN || variable === undefined) {
+        return true;
+    }
+    return false;
+}
+
+function checkAnswered() {
+    let currentQuestion = getCurrentQuestionFrame();
+    let inputs = currentQuestion.querySelectorAll("input");
+
+    let answered = false;
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].checked) {
+            answered = true;
+            break;
+        }
+    }
+    if (!answered) {
+        return;
+    }
+
+    let currentQuestionButton = getCurrentQuestionButton();
+    currentQuestionButton.classList.add("answered");
+}
+
+function markQuestion() {
+    let currentQuestionButton = getCurrentQuestionButton();
+
+    if (currentQuestionButton.classList.contains("marked")) {
+        currentQuestionButton.classList.remove("marked");
+    }
+    else {
+        currentQuestionButton.classList.add("marked");
+    }
+    checkMark();
+}
+
+function checkMark() {
+    let currentQuestionButton = getCurrentQuestionButton();
+
+    if (currentQuestionButton.classList.contains("marked")) {
+        markButton.innerText = "Снять флажок";
+    }
+    else {
+        markButton.innerText = "Отметить флажком";
+    }
+}
+
+function nextQuestion() {
+    let currentQuestion = getCurrentQuestionNumber();
+    goToQuestion(currentQuestion + 1);
+}
+
+function previousQuestion() {
+    let currentQuestion = getCurrentQuestionNumber();
+    goToQuestion(currentQuestion - 1);
+}
+
+function goToQuestion(number : number) {
+    let currentQuestion = document.querySelector(".right-column .question.selected");
+    let currentQuestionButton = document.querySelector(".left-column .select-button.selected");
+    let newQuestion = document.querySelector(".right-column .question[question='" + number + "']");
+    let newQuestionButton = document.querySelector(".left-column .select-button[question='" + number + "']");
+    if (newQuestion === null || currentQuestion === null
+        || currentQuestionButton === null || newQuestionButton === null)
+        return;
+
+    checkAnswered();
+    currentQuestion.classList.remove("selected");
+    currentQuestionButton.classList.remove("selected");
+    newQuestion.classList.add("selected");
+    newQuestionButton.classList.add("selected");
+    checkMark();
+}
+
+function endTest() {
+    let form = document.getElementsByTagName("form")[0];
+    let confirmation = confirm("Отправить результаты на проверку?");
+    if (confirmation) {
+        form.submit();
+    }
+}
+
+let markButton = document.getElementById("mark-question");
+markButton.onclick = markQuestion;
+
+let prevButton = document.getElementById("prev-question");
+let nextButton = document.getElementById("next-question");
+prevButton.onclick = previousQuestion;
+nextButton.onclick = nextQuestion;
+
+let buttons : any = document.querySelectorAll(".left-column nav .select-button");
+
+for (let i = 0; i < buttons.length; i++) {
+    let questionNumber = +buttons[i].getAttribute("question");
+    if (isNull(questionNumber))
+        continue;
+
+    buttons[i].onclick = function() {
+        goToQuestion(questionNumber);
+    }
+}
+
+let endTestButton : any = document.querySelector(".left-column .submit-button");
+endTestButton.onclick = endTest;
+
+let timer : HTMLElement = document.querySelector(".left-column .timer .time-left");
+let time = 60 * 60;
+setTimeout(function timerTick() {
+    time--;
+    let minutes = Math.floor(time / 60);
+    let seconds = (time - +minutes * 60) % 60;
+    let formatter = new Intl.NumberFormat("ru", {
+        minimumIntegerDigits: 2
+    })
+    timer.innerText = "" + minutes + ":" + formatter.format(seconds);
+    if (time > 0) {
+        setTimeout(timerTick, 1000);
+    }
+    else {
+        alert("Время вышло!");
+        let form = document.getElementsByTagName("form")[0];
+        form.submit();
+    }
+}, 1000)
